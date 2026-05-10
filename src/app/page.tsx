93dfbb14185097c731e8bcbe092564ea5f901c7a
Rohit Sharma <rohits8731@gmail.com>
2026-05-11 01:41:47 +0530

    02: Convex and Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
+"use client"
+
+import { RedirectToSignIn, UserButton } from '@clerk/nextjs'
+import { Authenticated, Unauthenticated } from 'convex/react';
 import React from 'react'
+
+function Content() {
+    return (
+        <div className="flex flex-col gap-y-4">
+            <div>
+                This is a screen for authenticated users only
+            </div>
+            <div>
+                <UserButton />
+            </div>
+        </div>
+    )
+}
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Authenticated>
+                <Content />
+            </Authenticated>
+            <Unauthenticated>
+                <RedirectToSignIn/>
+            </Unauthenticated>
+        </>
     )
 }
 

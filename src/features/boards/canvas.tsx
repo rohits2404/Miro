fb23b9a8779143c68a33a0371cf6bcd17316d516
Rohit Sharma <rohits8731@gmail.com>
2026-05-12 01:42:05 +0530

    16: Room Info

--- src/features/boards/canvas.tsx
+++ src/features/boards/canvas.tsx
@@ -5,7 +5,6 @@ import { Toolbar } from "./components/toolbar";
 import { Participants } from "./components/participants";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { RedirectToSignIn } from "@clerk/nextjs";
-import { useSelf } from "@liveblocks/react";
 
 interface CanvasProps {
     boardId: string;
@@ -15,15 +14,11 @@ export const Canvas = ({
     boardId,
 }: CanvasProps) => {
 
-    const info = useSelf((me) => me.info);
-
-    console.log(info)
-
     return (
         <>
             <Authenticated>
                 <main className="h-full w-full relative bg-neutral-100 touch-none">
-                    <Info />
+                    <Info boardId={boardId} />
                     <Participants />
                     <Toolbar />
                 </main>
--- src/features/boards/components/info.tsx
+++ src/features/boards/components/info.tsx
@@ -1,12 +1,97 @@
-export const Info = () => {
+"use client"
+
+import { useRenameModal } from "@/store/use-rename-modal";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
+
+interface InfoProps {
+    boardId: string;
+};
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
+const TabSeparator = () => {
     return (
-        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-            TODO: Information About The Board
+        <div className="text-neutral-300 px-1.5">
+            |
         </div>
     );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const Info = ({
+    boardId,
+}: InfoProps) => {
+  
+    const { onOpen } = useRenameModal();
+
+    const data = useQuery(api.board.get, {
+        id: boardId as Id<"boards">,
+    });
+
+    if (!data) return <InfoSkeleton />;
+
+    return (
+        <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+            <Hint label="Go To Boards" side="bottom" sideOffset={10}>
+                <Button asChild variant="board" className="px-2">
+                    <Link href="/">
+                        <Image
+                        src="/images/logo.svg"
+                        alt="Board logo"
+                        height={40}
+                        width={40}
+                        />
+                        <span className={cn(
+                        "font-semibold text-xl ml-2 text-black",
+                        font.className,
+                        )}>
+                            Board
+                        </span>
+                    </Link>
+                </Button>
+            </Hint>
+            <TabSeparator />
+            <Hint label="Edit title" side="bottom" sideOffset={10}>
+                <Button
+                variant="board"
+                className="text-base font-normal px-2"
+                onClick={() => onOpen(data._id, data.title)}
+                >
+                    {data.title}
+                </Button>
+            </Hint>
+            <TabSeparator />
+            <Actions
+                id={data._id}
+                title={data.title}
+                side="bottom"
+                sideOffset={10}
+            >
+                <div>
+                    <Hint label="Main menu" side="bottom" sideOffset={10}>
+                        <Button size="icon" variant="board">
+                            <Menu />
+                        </Button>
+                    </Hint>
+                </div>
+            </Actions>
+        </div>
+    );
+};
+
+export const InfoSkeleton = () => {
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-75"/>
     );

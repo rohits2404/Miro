63bc81dea054fff19a382692266e64f6ee3637bb
Rohit Sharma <rohits8731@gmail.com>
2026-05-12 02:08:12 +0530

    19: Canvas State

--- src/features/boards/canvas.tsx
+++ src/features/boards/canvas.tsx
@@ -5,6 +5,9 @@ import { Toolbar } from "./components/toolbar";
 import { Participants } from "./components/participants";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { RedirectToSignIn } from "@clerk/nextjs";
+import { useState } from "react";
+import { CanvasMode, type CanvasState } from "./types/canvas";
+import { useCanRedo, useCanUndo, useHistory } from "@liveblocks/react";
 
 interface CanvasProps {
     boardId: string;
@@ -14,13 +17,28 @@ export const Canvas = ({
     boardId,
 }: CanvasProps) => {
 
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
+
     return (
         <>
             <Authenticated>
                 <main className="h-full w-full relative bg-neutral-100 touch-none">
                     <Info boardId={boardId} />
                     <Participants />
-                    <Toolbar />
+                    <Toolbar
+                    canvasState={canvasState}
+                    setCanvasState={setCanvasState}
+                    canRedo={canRedo}
+                    canUndo={canUndo}
+                    undo={history.undo}
+                    redo={history.redo}
+                    />
                 </main>
             </Authenticated>
             <Unauthenticated>

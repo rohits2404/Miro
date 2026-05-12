4a6c556774aff969c444d09e02b967cd5e7bd7ad
Rohit Sharma <rohits8731@gmail.com>
2026-05-12 12:23:28 +0530

    28: Selection Net

--- src/features/boards/canvas.tsx
+++ src/features/boards/canvas.tsx
@@ -7,9 +7,9 @@ import { Participants } from "./components/participants";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { RedirectToSignIn } from "@clerk/nextjs";
 import { useCallback, useMemo, useState } from "react";
-import { Camera, CanvasMode, Color, LayerType, Point, Side, XYWH, type CanvasState } from "./types/canvas";
+import { Camera, CanvasMode, Color, LayerType, Point, Side, XYWH, type CanvasState, type Layer } from "./types/canvas";
 import { useCanRedo, useCanUndo, useHistory, useMutation, useOthersMapped, useStorage } from "@liveblocks/react";
-import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils";
+import { connectionIdToColor, findIntersectingLayersWithRectangle, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils";
 import { CursorsPresence } from "./components/cursors-presence";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./components/layer-preview";
@@ -112,6 +112,44 @@ export const Canvas = ({
         }
     }, []);
 
+    const updateSelectionNet = useMutation((
+        { storage, setMyPresence },
+        current: Point,
+        origin: Point,
+    ) => {
+        const layers = storage.get("layers");
+        setCanvasState({
+            mode: CanvasMode.SelectionNet,
+            origin,
+            current,
+        });
+
+        const ids = findIntersectingLayersWithRectangle(
+            layerIds,
+            layers as unknown as ReadonlyMap<string, Layer>,
+            origin,
+            current,
+        );
+
+        setMyPresence({ selection: ids });
+    }, [layerIds]);
+
+    const startMultiSelection = useCallback((
+        current: Point,
+        origin: Point,
+    ) => {
+        if (
+            Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5
+        ) {
+            setCanvasState({
+                mode: CanvasMode.SelectionNet,
+                origin,
+                current,
+            });
+        }
+    }, []);
+
+
     const resizeSelectedLayer = useMutation((
         { storage, self },
         point: Point,
@@ -161,7 +199,11 @@ export const Canvas = ({
 
         const current = pointerEventToCanvasPoint(e, camera);
 
-        if (canvasState.mode === CanvasMode.Translating) {
+        if (canvasState.mode === CanvasMode.Pressing) {
+            startMultiSelection(current, canvasState.origin);
+        } else if (canvasState.mode === CanvasMode.SelectionNet) {
+            updateSelectionNet(current, canvasState.origin);
+        } else if (canvasState.mode === CanvasMode.Translating) {
             translateSelectedLayers(current);
         } else if (canvasState.mode === CanvasMode.Resizing) {
             resizeSelectedLayer(current);
@@ -301,6 +343,15 @@ export const Canvas = ({
                             <SelectionBox
                             onResizeHandlePointerDown={onResizeHandlePointerDown}
                             />
+                            {canvasState.mode === CanvasMode.SelectionNet && canvasState.current != null && (
+                                <rect
+                                className="fill-blue-500/5 stroke-blue-500 stroke-1"
+                                x={Math.min(canvasState.origin.x, canvasState.current.x)}
+                                y={Math.min(canvasState.origin.y, canvasState.current.y)}
+                                width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+                                height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+                                />
+                            )}
                             <CursorsPresence />
                         </g>
                     </svg>
